ec84147c15c2e9ff3005e622e96850d74bee7082
gtallen13 <gtallenpadi13@gmail.com>
2022-03-09 11:18:08 -0600

    primer commit

---
+++ routes/api/v1/v1.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const { verifyApiHeaderToken } =
+  require('./headerVerifyMiddleware');
+
+const {passport, jwtMiddleware} = require('./seguridad/jwtHelper');
+
+// const pacientesRoutes = require('./pacientes/pacientes');
+// const seguridadRoutes = require('./seguridad/seguridad');
+// const expedientesRoutes = require('./expedientes/expedientes');
+router.use(passport.initialize());
+//public
+router.use('/seguridad', verifyApiHeaderToken, seguridadRoutes);
+//middlewares
+router.use(
+  '/pacientes',
+  verifyApiHeaderToken,
+  jwtMiddleware,
+  pacientesRoutes
+);
+// router.use('/expedientes', expedientesRoutes);
+module.exports = router;
