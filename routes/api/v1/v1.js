5410a27be5ae30109331ed30e13fa39d3b8586c8
Kilby Salgado <kir_salgadoc@unicah.edu>
2022-03-19 00:05:22 -0600

    declaracion de rutas del modulo de seguridad

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { verifyApiHeaderToken } = require('./headerVerifyMiddleware');
-
-//const {passport, jwtMiddleware} = require('./seguridad/jwtHelper');
+const {passport, jwtMiddleware} = require('./seguridad/jwtHelper');
 
 const preguntasRoutes = require('./preguntas/preguntas');
-//const seguridadRoutes = require('./seguridad/seguridad');
-// const expedientesRoutes = require('./expedientes/expedientes');
-//router.use(passport.initialize());
+const seguridadRoutes = require('./seguridad/seguridad');
+
+router.use(passport.initialize());
 //public
-//router.use('/seguridad', seguridadRoutes);
+router.use('/seguridad', seguridadRoutes);
 //middlewares
 router.use(
   '/preguntas',
   verifyApiHeaderToken,
- // jwtMiddleware,
+  jwtMiddleware,
   preguntasRoutes
 );
 // router.use('/expedientes', expedientesRoutes);
