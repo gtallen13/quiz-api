229295af02f9991e64d8f801f5d5fd1d52b0c057
gtallen13 <gtallenpadi13@gmail.com>
2022-03-31 11:47:10 -0600

    disenio pagina revisar

--- quiz-api-fe/src/App.js
+++ quiz-api-fe/src/App.js
@@ -7,6 +7,7 @@ import Splash from './Components/Views/Splash';
 import Signin from './Components/Views/Signin/SigninPage';
 import Login from './Components/Views/Login/LoginPage';
 import Todo from './Components/Views/Todo/TodoPage';
+import Revision from './Components/Views/Revision/RevisionPage';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/todo" element={<Todo />} />
+        <Route path="/revision" element={<Revision/>}/>
       </Routes>
     </BrowserRouter>
     </Provider>
